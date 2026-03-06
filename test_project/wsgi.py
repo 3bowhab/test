01d13d710ab08c8bc951c0c2644dc6bdd70ab02a
ali ibrahim <151842868+3bowhab@users.noreply.github.com>
2026-03-06 15:16:52 +0200

    Add files via upload

---
+++ test_project/wsgi.py
@@ -0,0 +1,24 @@
+"""
+WSGI config for test_project project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_project.settings')
+
+application = get_wsgi_application()
+
+try:
+    from django.core.management import call_command
+    call_command('migrate', interactive=False)
+except Exception as e:
+    print(f"Migration failed: {e}")
+
+app = application
