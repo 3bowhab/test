9be0fe8a8628debea10053679ca5678600234aaf
3bowhab <ali.abdelwhab21@gmail.com>
2026-03-13 17:20:30 +0200

    edit neon database

--- test_project/wsgi.py
+++ test_project/wsgi.py
@@ -15,5 +15,10 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_project.settings')
 
 application = get_wsgi_application()
 
+try:
+    from django.core.management import call_command
+    call_command('migrate', interactive=False)
+except Exception as e:
+    print(f"Migration failed: {e}")
 
 app = application
